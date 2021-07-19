8f2838430dca6e97143cf7a94ecf22dda9d31144
ejonghwan <sun87-1@daum.net>
2021-07-20 00:21:53 +0900

    a

--- 04_rotate_shape/app.js
+++ 04_rotate_shape/app.js
@@ -21,7 +21,7 @@ class App {
         this.offsetX = 0
         document.addEventListener('pointerdown', this.onDown.bind(this))
         document.addEventListener('pointermove', this.onMove.bind(this))
-        document.addEventListener('pointerUp', this.onUp.bind(this))
+        document.addEventListener('pointerup', this.onUp.bind(this))
 
         window.requestAnimationFrame(this.animate.bind(this))
     }
@@ -37,9 +37,9 @@ class App {
 
         this.polygon = new Polygon(
             this.stageWidth / 2,
-            this.stageHeight / 2,
-            this.stageHeight / 4,
-            3
+            this.stageHeight + (this.stageHeight / 4),
+            this.stageHeight / 1.5,
+            15
         )
 
     }
@@ -50,12 +50,14 @@ class App {
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight )
         this.moveX *= 0.92
         this.polygon.animate(this.ctx, this.moveX)
+        
     }
 
     onDown(e) {
         this.isDown = true;
         this.moveX = 0;
         this.offsetX = e.clientX;
+        console.log('down')
         
     }
 
@@ -63,11 +65,13 @@ class App {
         if(this.isDown) {
             this.moveX = e.clientX - this.offsetX; //무브한값에서 처음값 뺴기
             this.offsetX = e.clientX; //
+            console.log('mvoe')
         }
     }
 
     onUp(e) {
         this.isDown = false;
+        console.log('up')
     }
 
 
--- 04_rotate_shape/polygon.js
+++ 04_rotate_shape/polygon.js
@@ -1,4 +1,21 @@
 const PI2 = Math.PI * 2
+const COLORS = [
+    "#4b45ab",
+    "#554fb8",
+    "#605ac7",
+    "#2a91a8",
+    "#2e9ab2",
+    "#32a5bf",
+    "#81b144",
+    "#85b944",
+    "#8fc549",
+    "#e0af27",
+    "#eeba2a",
+    "#fec72e",
+    "#bf342d",
+    "#ca3931",
+    "#d7423a",
+  ];
 
 export class Polygon {
     constructor(x, y, radius, sides) {
@@ -13,15 +30,16 @@ export class Polygon {
 
     animate(ctx, moveX) {
         ctx.save();
-        ctx.fillStyle = '#000';
-        ctx.beginPath();
+        // ctx.fillStyle = '#000';
+        // ctx.beginPath();
 
         const angle = PI2 / this.sides; //360도 3등분
+        const angle2 = PI2 / 4
 
         ctx.translate(this.x, this.y) // 중앙으로 이동
 
 
-        this.rotate -= moveX * 0.008;
+        this.rotate -= moveX * 0.08;
         ctx.rotate(this.rotate)
 
 
@@ -29,11 +47,10 @@ export class Polygon {
             const x =  this.radius * Math.cos(angle * i); //음수에 *0하면 1
             const y =  this.radius * Math.sin(angle * i);
 
-            (i === 0) ? ctx.moveTo(x, y) : ctx.lineTo(x, y)
+            // (i === 0) ? ctx.moveTo(x, y) : ctx.lineTo(x, y)
             
-            console.log(`${i} x: ${x}`)
-            console.log(`${i} y: ${y}`)
-
+            // console.log(`${i} x: ${x}`)
+            // console.log(`${i} y: ${y}`)
             /*
             0 x: 229.25
             0 y: 0
@@ -43,10 +60,33 @@ export class Polygon {
             2 y: -198.5363238175825
             */
 
-        }
 
-        ctx.fill();
-        ctx.closePath();
+            // ctx.beginPath();
+            // ctx.arc(x, y, 30, 0, PI2, false)
+            // ctx.fill()
+
+            // 두번쨰
+            ctx.save()
+            ctx.fillStyle = COLORS[i]
+            ctx.translate(x, y);
+            ctx.rotate(((360 / this.sides) * i + 45) * Math.PI / 180)
+            ctx.beginPath();
+
+            for(let j = 0; j < 4; j++) {
+                const x2 = 130 * Math.cos(angle2 * j);
+                const y2 = 130 * Math.sin(angle2 * j);
+                (j === 0) ? ctx.moveTo(x2, y2) : ctx.lineTo(x2, y2)
+            }
+
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore()
+
+        }
+        
+
+        // ctx.fill();
+        // ctx.closePath();
         ctx.restore();
     }
 
