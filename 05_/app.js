59db8e58ba18697bcdff7699e436bc12d3b996a3
ejonghwan <sun87-1@daum.net>
2021-07-26 23:56:42 +0900

    a

--- 05_/app.js
+++ 05_/app.js
@@ -1,19 +1,55 @@
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas')
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
 
-let arr = ['a', 'b', 'c', 'd', 'e', 'f']
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
 
-let prev = arr[0];
-let cx = null;
+        this.isLoaded = false;
+        this.imgPos = {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        }
 
-for(let i = 1; i < arr.length; i++) {
-    
-    cx = prev
-    console.log('cx: ', cx + 1)
+        this.image = new Image()
+        this.image.src = '/image493_2.jpg'
+        this.image.onload = () => {{
+            this.isLoaded = true;
+            this.drawImage();
+        }}
+    }
 
-    prev = arr[i]
-    console.log(prev)
+    resize() {
+        this.deviceWidth = document.body.clientWidth
+        this.deviceHeight = document.body.clientHeight
+        this.canvas.width = this.deviceWidth * this.pixelRatio
+        this.canvas.height = this.deviceHeight * this.pixelRatio
+        this.ctx.scale(this.pixelRatio, this.pixelRatio)
 
-    
+        if(this.isLoaded) {
+            this.drawImage();
+        }
+    }
 
+    drawImage() {
+        const stageRatio = this.stageWidth / this.stageWidth;
+        const imgRatio = this.image.width / this.image.height;
 
+        this.imgPos.width = this.stageWidth;
+        this.imgPos.height = this.stageHeight;
 
+        if(imgRatio > stageRatio) {
+            this.imgPos.width = Math.round(this.image.width * (this.stageHeight / this.image.height))
+        } else {
+            
+        }
+    }
 }
+
+
+window.onload = new App()
