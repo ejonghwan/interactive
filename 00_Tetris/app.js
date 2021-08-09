a43a86bfc61c50edf4832ca1ceaf4f40bd6d54a8
ejonghwan <sun87-1@daum.net>
2021-08-09 23:01:46 +0900

    a

---
+++ 00_Tetris/app.js
@@ -0,0 +1,76 @@
+
+//dom
+const ground = document.querySelector('.ground > ul')
+
+
+// setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+
+
+//variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+    tree: [
+        [[0, 0], [0, 1], [1, 0], [1, 1]],
+        [],
+        [],
+        [],
+    ]
+}
+
+const movingItem = {
+    type: "tree",
+    direction: 0,
+    top: 0,
+    left: 0,
+
+};
+
+
+
+//functions
+init();
+
+
+function init() {
+
+    tempMovingItem = {...movingItem};
+
+    for(let i = 0; i < GAME_ROWS; i++) {
+        prependNewLine()
+    }
+
+    randerBlocks()
+     
+}
+
+
+
+function prependNewLine() {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+    for(let j = 0; j < GAME_COLS; j++) {
+        const matrix = document.createElement('li');
+        ul.prepend(matrix);
+    }
+    li.prepend(ul);
+    ground.prepend(li)
+}
+
+
+function randerBlocks() {
+    const { type, direction, top, left } = tempMovingItem;
+    
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0];
+        const y = block[1];
+        const target = ground.childNodes[y].childNodes[0].childNodes[x];
+        target.classList.add(type)
+        console.log(target)
+    })
+}
