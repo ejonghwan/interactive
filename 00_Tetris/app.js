f8d9452063203c05b3fee38470cc4affacaf2d4c
ejonghwan <sun87-1@daum.net>
2021-08-10 00:42:39 +0900

    a

--- 00_Tetris/app.js
+++ 00_Tetris/app.js
@@ -16,7 +16,7 @@ let tempMovingItem;
 
 const BLOCKS = {
     tree: [
-        [[0, 0], [0, 1], [1, 0], [1, 1]],
+        [[2, 1], [0, 1], [1, 0], [1, 1]],
         [],
         [],
         [],
@@ -27,7 +27,7 @@ const movingItem = {
     type: "tree",
     direction: 0,
     top: 0,
-    left: 0,
+    left: 3,
 
 };
 
@@ -38,15 +38,13 @@ init();
 
 
 function init() {
-
     tempMovingItem = {...movingItem};
 
     for(let i = 0; i < GAME_ROWS; i++) {
         prependNewLine()
     }
 
-    randerBlocks()
-     
+    renderBlocks()
 }
 
 
@@ -54,23 +52,83 @@ function init() {
 function prependNewLine() {
     const li = document.createElement('li');
     const ul = document.createElement('ul');
+    ground.prepend(li)
+    li.prepend(ul);
+    
+
     for(let j = 0; j < GAME_COLS; j++) {
         const matrix = document.createElement('li');
         ul.prepend(matrix);
     }
-    li.prepend(ul);
-    ground.prepend(li)
-}
-
-
-function randerBlocks() {
-    const { type, direction, top, left } = tempMovingItem;
     
-    BLOCKS[type][direction].forEach(block => {
-        const x = block[0];
-        const y = block[1];
-        const target = ground.childNodes[y].childNodes[0].childNodes[x];
-        target.classList.add(type)
-        console.log(target)
-    })
 }
+
+
+function renderBlocks() {
+    const { type, direction, top, left } = tempMovingItem;
+    const movingBlocks = document.querySelectorAll('.moving')
+
+    movingBlocks.forEach( moving => {
+        console.log(moving)
+        moving.classList.remove(type, "moving")
+    })
+
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0] + left;
+        const y = block[1] + top;
+        const target = ground.childNodes[y] ? ground.childNodes[y].childNodes[0].childNodes[x] : null;
+        const isAvailable = checkEmpty(target)
+
+        if(isAvailable) {
+            target.classList.add(type, "moving")
+        } else {
+            tempMovingItem = {...movingItem} //원상복귀
+            setTimeout(() => { //모든 스택이 비웠을 때
+                renderBlocks()
+            }, 0)
+        }
+
+        // console.log(target)
+    })
+
+    movingItem.left = left
+    movingItem.top = top;
+    movingItem.direction = direction
+}
+
+function checkEmpty(target) {
+    if(!target) {
+        return false
+    };
+
+    return true;
+}
+
+function moveBlock(moveType, amount) {
+    tempMovingItem[moveType] += amount;
+    renderBlocks()
+}
+
+
+window.addEventListener('keydown', e => {
+    switch(e.keyCode) {
+        case 39: {
+            return moveBlock('left', 1)
+            // break
+        }
+
+        case 37: {
+            return moveBlock('left', -1)
+            // break
+        }
+
+        case 40: {
+            return moveBlock('top', 1)
+        }
+
+
+        default: {
+            break
+        }
+    }
+})
