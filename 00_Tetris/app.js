43b1e95149a3217cf8ec2448e01eeb3cc9fb25bc
ejonghwan <sun87-1@daum.net>
2021-08-15 23:56:27 +0900

    a

--- 00_Tetris/app.js
+++ 00_Tetris/app.js
@@ -1,213 +1,213 @@
-// import { BLOCKS } from './Blocks.js'
+import { BLOCKS } from './Blocks.js'
 
-// //dom
-// const ground = document.querySelector('.ground > ul')
-
-
-// // setting
-// const GAME_ROWS = 20;
-// const GAME_COLS = 10;
+//dom
+const ground = document.querySelector('.ground > ul')
 
 
-// //variables
-// let score = 0;
-// let duration = 500;
-// let downInterval;
-// let tempMovingItem;
+// setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
 
 
-
-// const movingItem = {
-//     type: "tree",
-//     direction: 0,
-//     top: 0,
-//     left: 3,
-
-// };
+//variables
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
 
 
 
-// //functions
-// init();
+const movingItem = {
+    type: "tree",
+    direction: 0,
+    top: 0,
+    left: 3,
+
+};
 
 
-// function init() {
-//     tempMovingItem = {...movingItem};
 
-//     for(let i = 0; i < GAME_ROWS; i++) {
-//         prependNewLine()
-//     }
-//     // renderBlocks()
-//     blockGenerater()
+//functions
+init();
+
+
+function init() {
+    tempMovingItem = {...movingItem};
+
+    for(let i = 0; i < GAME_ROWS; i++) {
+        prependNewLine()
+    }
+    // renderBlocks()
+    blockGenerater()
 
  
-// }
+}
 
 
 
-// function prependNewLine() {
-//         const li = document.createElement('li');
-//         const ul = document.createElement('ul');
-//         ground.prepend(li)
-//         li.prepend(ul);
+function prependNewLine() {
+        const li = document.createElement('li');
+        const ul = document.createElement('ul');
+        ground.prepend(li)
+        li.prepend(ul);
 
-//         for(let j = 0; j < GAME_COLS; j++) {
-//             const matrix = document.createElement('li');
-//             ul.prepend(matrix);
-//         }
-// }
+        for(let j = 0; j < GAME_COLS; j++) {
+            const matrix = document.createElement('li');
+            ul.prepend(matrix);
+        }
+}
 
 
-// function renderBlocks(moveType = '') {
-//     const { type, direction, top, left } = tempMovingItem;
-//     const movingBlocks = document.querySelectorAll('.moving')
+function renderBlocks(moveType = '') {
+    const { type, direction, top, left } = tempMovingItem;
+    const movingBlocks = document.querySelectorAll('.moving')
 
-//     movingBlocks.forEach( moving => {
-//         // console.log(moving)
-//         moving.classList.remove(type, "moving")
-//     })
+    movingBlocks.forEach( moving => {
+        // console.log(moving)
+        moving.classList.remove(type, "moving")
+    })
 
-//     BLOCKS[type][direction].some(block => {
-//         const x = block[0] + left;
-//         const y = block[1] + top;
-//         const target = ground.childNodes[y] ? ground.childNodes[y].childNodes[0].childNodes[x] : null;
-//         const isAvailable = checkEmpty(target)
+    BLOCKS[type][direction].some(block => {
+        const x = block[0] + left;
+        const y = block[1] + top;
+        const target = ground.childNodes[y] ? ground.childNodes[y].childNodes[0].childNodes[x] : null;
+        const isAvailable = checkEmpty(target)
 
-//         if(isAvailable) {
-//             target.classList.add(type, "moving")
-//         } else {
-//             tempMovingItem = {...movingItem} //원상복귀
-//             setTimeout(() => { //모든 스택이 비웠을 때
-//                 renderBlocks('retry')
-//                 if(moveType === 'top') {
-//                     seizeBlock();
-//                 }
-//             }, 0)
-//             return true; //forEach 대신 some return true를 쓴 이유는 ? else에 걸렸을 때 한개의 블럭만 걸리게. 기본적으로 반복은 블록의 개수만큼 돌리기 때문
-//         }
+        if(isAvailable) {
+            target.classList.add(type, "moving")
+        } else {
+            tempMovingItem = {...movingItem} //원상복귀
+            setTimeout(() => { //모든 스택이 비웠을 때
+                renderBlocks('retry')
+                if(moveType === 'top') {
+                    seizeBlock();
+                }
+            }, 0)
+            return true; //forEach 대신 some return true를 쓴 이유는 ? else에 걸렸을 때 한개의 블럭만 걸리게. 기본적으로 반복은 블록의 개수만큼 돌리기 때문
+        }
 
-//         // console.log(target)
-//     })
+        // console.log(target)
+    })
 
-//     movingItem.left = left
-//     movingItem.top = top;
-//     movingItem.direction = direction
-// }
+    movingItem.left = left
+    movingItem.top = top;
+    movingItem.direction = direction
+}
 
-// function seizeBlock() {
-//     console.log('seize block')
-//     const movingBlocks = document.querySelectorAll('.moving')
+function seizeBlock() {
+    console.log('seize block')
+    const movingBlocks = document.querySelectorAll('.moving')
 
-//     movingBlocks.forEach( moving => {
-//         // console.log(moving)
-//         moving.classList.remove("moving")
-//         moving.classList.add("seized")
-//     })
+    movingBlocks.forEach( moving => {
+        // console.log(moving)
+        moving.classList.remove("moving")
+        moving.classList.add("seized")
+    })
 
-//     // blockGenerater()
-//     checkMatch();
-// }
+    // blockGenerater()
+    checkMatch();
+}
 
-// function checkMatch() {
+function checkMatch() {
 
-//     const childNodes = ground.childNodes;
-//     childNodes.forEach(child => {
-//         let matched = true;
-//         child.children[0].childNodes.forEach(li => {
+    const childNodes = ground.childNodes;
+    childNodes.forEach(child => {
+        let matched = true;
+        child.children[0].childNodes.forEach(li => {
 
-//             if(!li.classList.contains('seized')) {
-//                 matched = false;
-//             }
-//         })
-//         if(matched) {
-//             child.remove();
-//             prependNewLine()
-//         }
+            if(!li.classList.contains('seized')) {
+                matched = false;
+            }
+        })
+        if(matched) {
+            child.remove();
+            prependNewLine()
+        }
        
-//     })
+    })
 
-//     blockGenerater()
-// }
+    blockGenerater()
+}
 
 
-// function blockGenerater() {
+function blockGenerater() {
 
-//     clearInterval(downInterval)
-//     downInterval = setInterval(() => {
-//         moveBlock('top', 1)
-//     }, duration)
+    clearInterval(downInterval)
+    downInterval = setInterval(() => {
+        moveBlock('top', 1)
+    }, duration)
 
-//     const obj = Object.entries(BLOCKS)
-//     const randomIndex = Math.floor(Math.random() * obj.length)
+    const obj = Object.entries(BLOCKS)
+    const randomIndex = Math.floor(Math.random() * obj.length)
     
 
-//     movingItem.type = obj[randomIndex][0]
-//     movingItem.top = 0;
-//     movingItem.left = 3;
-//     movingItem.direction = 0;
-//     tempMovingItem = {...movingItem}
-//     renderBlocks();
+    movingItem.type = obj[randomIndex][0]
+    movingItem.top = 0;
+    movingItem.left = 3;
+    movingItem.direction = 0;
+    tempMovingItem = {...movingItem}
+    renderBlocks();
 
-// }
+}
 
-// function checkEmpty(target) {
-//     if(!target || target.classList.contains('seized')) {
-//         return false
-//     };
+function checkEmpty(target) {
+    if(!target || target.classList.contains('seized')) {
+        return false
+    };
 
-//     return true;
-// }
+    return true;
+}
 
-// function moveBlock(moveType, amount) {
-//     tempMovingItem[moveType] += amount;
-//     renderBlocks(moveType)
-// }
+function moveBlock(moveType, amount) {
+    tempMovingItem[moveType] += amount;
+    renderBlocks(moveType)
+}
 
-// function changeDirction() {
+function changeDirction() {
 
-//     const direction = tempMovingItem.direction;
-//     direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1
-//     renderBlocks()
-// }
+    const direction = tempMovingItem.direction;
+    direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction += 1
+    renderBlocks()
+}
 
-// function dropBlock() {
-//     clearInterval(downInterval)
-//     downInterval = setInterval(() => {
-//         moveBlock('top', 1)
-//     }, 10)
-// }
+function dropBlock() {
+    clearInterval(downInterval)
+    downInterval = setInterval(() => {
+        moveBlock('top', 1)
+    }, 10)
+}
 
-// window.addEventListener('keydown', e => {
-//     switch(e.keyCode) {
-//         case 39: {
-//             return moveBlock('left', 1)
-//             break
-//         }
+window.addEventListener('keydown', e => {
+    switch(e.keyCode) {
+        case 39: {
+            return moveBlock('left', 1)
+            break
+        }
 
-//         case 37: {
-//             return moveBlock('left', -1)
-//             break
-//         }
+        case 37: {
+            return moveBlock('left', -1)
+            break
+        }
 
-//         case 40: {
-//             return moveBlock('top', 1)
-//             break
-//         }
+        case 40: {
+            return moveBlock('top', 1)
+            break
+        }
 
-//         case 38: {
-//             return changeDirction()
-//             break
-//         }
+        case 38: {
+            return changeDirction()
+            break
+        }
 
-//         case 32: {
-//             return dropBlock()
-//             break
-//         }
+        case 32: {
+            return dropBlock()
+            break
+        }
 
 
 
-//         default: {
-//             break
-//         }
-//     }
-// })
+        default: {
+            break
+        }
+    }
+})
